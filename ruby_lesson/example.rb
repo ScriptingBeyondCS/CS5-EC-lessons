963a323dab0bb01dc063b10c4ba82b0ee4340b37
hhoffman14 <hannahoffman14@gmail.com>
2017-06-08 16:43:23 -0700

    added Ruby lesson

---
+++ ruby_lesson/example.rb
@@ -0,0 +1,45 @@
+class Cake
+    def initialize(ingredients = [])
+        @ingredients = ingredients
+    end
+    
+    def add_ingredients(new_ingredient)
+        if @ingredients.nil?
+            @ingredients = [new_ingredient]
+        else
+            @ingredients += [new_ingredient]
+        end
+        puts new_ingredient + " added!!"
+    end
+    
+    def list_ingredients
+        puts "listing ingredients.."        
+        if @ingredients != []
+            @ingredients.each {|element| 
+            sleep(0.5)
+            puts element
+        }
+        else
+            puts "no ingredients yet!"
+            puts "please add some.."
+        end
+    end
+    
+    def bake
+        puts "baking cake..."
+        sleep(2)
+        puts "it's done!"
+    end
+end
+
+if __FILE__==$0
+    cake = Cake.new
+    cake.list_ingredients
+    ingredients = ["sugar","flour","eggs","vanilla"]
+    ingredients.each {|ingredient| 
+    sleep(0.5)
+    cake.add_ingredients(ingredient)
+}
+    cake.list_ingredients
+    cake.bake
+end
