88e04ca252ad7ed1f3c2a343e52860ecfe8d93cd
hhoffman14 <hannahoffman14@gmail.com>
2017-07-25 15:46:48 -0700

    removed MATLAB answers

--- MATLAB_lesson/answer_code.m
+++
@@ -1,9 +0,0 @@
-load cities.mat;
-health = ratings(:,3);
-arts = ratings(:,7);
-plot(health, arts, '*');
-xlabel('health ratings');
-ylabel('arts ratings');
-fit_line = fit(health, arts, 'poly1');
-hold on;
-plot(fit_line);
